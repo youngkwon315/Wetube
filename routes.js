a92af86fe5a09112011ff14efa35f863a243308a
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2019-12-23 23:14:48 +0900

    191223 Changed
    1. Finished chapter 3
    2. added video and user detail

--- routes.js
+++ routes.js
@@ -9,9 +9,9 @@ const SEARCH = "/search";
 // Users
 
 const USERS = "/users";
+const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/editProfile";
 const CHANGE_PASSWORD = "/changePassword";
-const USER_DETAIL = "/:id";
 
 // Videos
 
@@ -28,12 +28,24 @@ const routes = {
   logout: LOGOUT,
   search: SEARCH,
   users: USERS,
-  userDetail: USER_DETAIL,
+  userDetail: id => {
+    if (id) {
+      return `/users/${id}`;
+    } else {
+      return USER_DETAIL;
+    }
+  },
   editProfile: EDIT_PROFILE,
   changePassword: CHANGE_PASSWORD,
   videos: VIDEOS,
   upload: UPLOAD,
-  videoDetail: VIDEO_DETAIL,
+  videoDetail: id => {
+    if (id) {
+      return `/videos/${id}`;
+    } else {
+      return VIDEO_DETAIL;
+    }
+  },
   editVideo: EDIT_VIDEO,
   deleteVideo: DELETE_VIDEO
 };
