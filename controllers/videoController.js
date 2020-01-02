8fa38fc9b761388dcc9ca0238470642310f33999
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2020-01-02 22:59:12 +0900

    200102 Changes
    1. Added Video models to DB

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,8 @@
 import routes from "../routes";
+import Video from "../models/Video";
 
 export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+  res.render("home", { pageTitle: "Home", videos: [] });
 };
 
 export const search = (req, res) => {
