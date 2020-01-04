944a5bc9a1b155fda737458c6ee1e7bf1d12e51f
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2020-01-04 14:13:33 +0900

    20200104 Changes
    1. Created Video uploads
    2. Used multer to succesfully upload videos

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,14 @@
 import routes from "../routes";
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos: [] });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 
 export const search = (req, res) => {
@@ -13,11 +19,18 @@ export const search = (req, res) => {
 };
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, description }
+    body: { title, description },
+    file: { path }
   } = req;
-  res.redirect(routes.videoDetail(324393));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+  });
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
