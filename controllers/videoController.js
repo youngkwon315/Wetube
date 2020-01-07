9096cdbdfd7f9977ee692e53169c3a69b0995569
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2020-01-07 23:54:48 +0900

    20200107 Changes
    1. Added eslint (not working)
    2. Developed Searching videos

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ _id: -1 });
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
@@ -11,10 +11,18 @@ export const home = async (req, res) => {
   }
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" }
+    });
+  } catch (error) {
+    console.log(error);
+  }
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 export const getUpload = (req, res) =>
@@ -74,6 +82,8 @@ export const deleteVideo = async (req, res) => {
   } = req;
   try {
     await Video.findOneAndRemove({ _id: id });
-  } catch (error) {}
+  } catch (error) {
+    console.log("dfdf");
+  }
   res.redirect(routes.home);
 };
