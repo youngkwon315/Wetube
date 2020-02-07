a3d14585a590fa9a20f192d6bbb438de82c96cda
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2020-02-07 17:06:10 +0900

    20200207 Changes
    1. Added comment API
    2. Fixed some css to add comment to the video
    * need to fix register view part

--- routes.js
+++ routes.js
@@ -33,6 +33,7 @@ const FB_CALLBACK = "/auth/facebook/callback";
 //API
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
 
 const routes = {
   home: HOME,
@@ -79,7 +80,8 @@ const routes = {
   facebook: FB,
   facebookCallback: FB_CALLBACK,
   api: API,
-  registerView: REGISTER_VIEW
+  registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT
 };
 
 export default routes;
