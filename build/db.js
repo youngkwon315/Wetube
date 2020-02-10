1dee940585147244ff9172e217ca8d1656fa8e70
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2020-02-11 01:49:35 +0900

    wetube

---
+++ build/db.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_dotenv["default"].config();
+
+_mongoose["default"].connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+var db = _mongoose["default"].connection;
+
+var handleOpen = function handleOpen() {
+  return console.log("Connected to DB");
+};
+
+var handleError = function handleError(error) {
+  return console.log("Error on DB Connection:".concat(error));
+};
+
+db.once("open", handleOpen);
+db.on("error", handleError);
---
+++ build/models/Comment.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var CommentSchema = new _mongoose["default"].Schema({
+  text: {
+    type: String,
+    required: "Text is required"
+  },
+  createdAt: {
+    type: Date,
+    "default": Date.now
+  }
+});
+
+var model = _mongoose["default"].model("Comment", CommentSchema);
+
+var _default = model;
+exports["default"] = _default;
